8246cad1d405de5caafe275554f030f52a035dc1
vikingviolinist <misopetrik@gmail.com>
2021-11-23 08:56:27 +0100

    Fix isQuizEnd logic

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,7 @@ class Quiz extends Component {
 
   render() {
     const isQuizEnd =
-      this.state.quiz_position === quizData.quiz_questions.length;
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
         {isQuizEnd ? (
