4b9a15d2102c5f4b082884aa6730ba208ac7afb9
vikingviolinist <misopetrik@gmail.com>
2021-11-23 09:43:49 +0100

    Add error handling

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
   handleClick(buttonText) {
-    if (buttonText === this.props.quiz_question.answer)
+    if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
+      this.setState({ incorrectAnswer: false });
+    } else this.setState({ incorrectAnswer: true });
   }
 
   render() {
     return (
       <main>
+        {this.state.incorrectAnswer && (
+          <p className="error">Sorry, that's not right</p>
+        )}
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
